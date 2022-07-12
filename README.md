4d07df7ef13f12c17d37cf256c980e6c846236e1
Samar Barakat <samarbarakat23@gmail.com>
2022-07-12 15:26:15 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## samkitkat cli business card
+
+![try npx samkitkat in your terminal to see this!](/samkitkatcli.PNG)
+
+
+
+
+ty to @whitep4nth3r for this wonderful tutorial! Make your own [here](https://whitep4nth3r.com/blog/build-a-business-card-cli-tool/)
+
---
+++ index.mjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+
+  const log = console.log;
+
+  const intro = "\t|\tHello there! Introducing myself through\t\t|\n\t|\tYOUR command line is I, Samar! Let’s\t\t|\n\t|\tchat sometime!\t\t\t\t\t|";
+
+  log(chalk.hex("#EDAFB8")("\n\n\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"));
+  log("\t|                                                       |")
+  log(chalk.bold(intro));
+  log("\t|                                                       |")
+  log(chalk.hex("#669BBC")("\t|\tTwitch.tv\t\t\t") + "/samkitkat\t|");
+  log(chalk.hex("#669BBC")("\t|\tTwitter.com\t\t\t") + "/samkitkat_\t|");
+  log(chalk.hex("#669BBC")("\t|\tTiktok.com\t\t\t") + "/@samarkitkat\t|");
+  log(chalk.hex("#669BBC")("\t|\tYoutube.com/c\t\t") + chalk.hex("#FFE26C")("`::`    /samkitkat\t|"));
+  log(chalk.hex("#FFE26C")("\t|\t                          \\                     |"));
+  log(chalk.hex("#FFE26C")("\t|\t               `::`         `                   |"));
+  log(chalk.hex("#FFE26C")("\t|\t        wWWWw   /              wWWWw          wWWWw"));
+  log(chalk.hex("#EDAFB8")("\t|\t vVVVv  (___)  wWWWw           (___)  vVVVv   (___)"));
+  log(chalk.hex("#EDAFB8")("\t|\t (___)   ~Y~   (___)   vVVVv    ~Y~   (___)    ~Y~"));
+  log(chalk.hex("#EDAFB8")("\t|\t  ~Y~     \|     ~Y~    (___)     |/    ~Y~      |/"));
+  log(chalk.hex("#52b69a")("\t|\t  \\|     \\|/    \\| /   \\~Y~/    \\|    \\ |/     \\|"));
+  log(chalk.hex("#52b69a")("\t|\t \\\\|//  \\\\|//  \\\\|///  \\\\|//   \\\\|//  \\\\|///  \\\\|//"));
+  log(chalk.hex("#689689")("\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"));
+  log("\n\n");
